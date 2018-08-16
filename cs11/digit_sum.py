8548e07b673deeb54c53b047d232166aeffdf4a3
mimin <optimusupperman@gmail.com>
2018-08-16 19:45:53 +0800

    added the digit_sum.py and largest.py

---
+++ cs11/digit_sum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+N = argv[1]
+
+result = 0
+length = 0
+index = 0
+
+while (length != 1):
+    length = len(N)
+    index = 0
+
+    N = str(result)
+
+print(str(N))
