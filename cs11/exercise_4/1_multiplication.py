d5a4d4d342bde4be32ccbd89b96ef18312356ad0
mimin <42409359+mimin@users.noreply.github.com>
2018-09-11 11:57:51 +0800

    Update 1_multiplication.py

--- cs11/exercise_4/1_multiplication.py
+++ cs11/exercise_4/1_multiplication.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
 n = int(input())
+curr = 1
 
-if n <
+if (n <= 0):
+  pass
+else:
+  for i in range (1,n+1):
+
+    for j in range (curr, i+1):
+        
+      for k in range(1,i+1):
+        print(curr * k, end='\t')
+
+      print('\n')
+      curr += 1
+    
+    curr = 1
+    print('\n' * 2)
