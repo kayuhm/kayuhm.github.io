61210c61d75e4d0b03dcfa045e006f0c3377a5c6
mimin <42409359+mimin@users.noreply.github.com>
2018-09-12 02:09:41 -0700

    finished pyramid function

--- cs11/exercise_4/3_pyramid.py
+++ cs11/exercise_4/3_pyramid.py
@@ -2,43 +2,33 @@
 
 from math import ceil
 
-def main():
-  t = int(input())
-  if t <= 0:
-    pass # temporary
-  else:
-    # variable declarations
-    array = []
-    h = 0
-    for i in range(t):
-      h = int(input())
-      if h <= 0:
-        return
-      else:
-        array.append(h)
-
-    for element in array:
-      pyramid(element)
+def build():
+    pass
 
 def pyramid(dimension):
-  width = dimension * 2 - 1
-  total_width = width ** 2
-  total_mid = ceil(total_width / 2)
-  middles = [total_mid]
+    width = dimension * 2 - 1
+    total_width = width ** 2
+    total_mid = ceil(total_width/2)
+    counter = 0
+    middles = [total_mid]
 
-  for big_row in range(dimension):
-    for row in range(dimension):
-      for col in range(total_width):
-        for element in middles:
-          if element - 1 - row <= col <= element - 1 + row:
-            print('*', end="")
-          else:
-            print(' ', end="")
-      print(' ')
+    for big_row in range(dimension):
+        if big_row > 0:
+            middles.append(total_mid - width * big_row)
+            middles.append(total_mid + width * big_row)
+            middles.sort()
 
-    if big_row != 0:
-      middles.append(total_mid - big_row * width)
-      middles.append(total_mid + big_row * width)
+        for row in range(dimension):
+            for col in range(total_width):
+                counter = 0
+                for mid in middles:
+                    if col < mid - 1 - row:
+                        print(' ', end='')
+                        break
+                    elif col <= mid - 1 + row:
+                        print('*', end='')
+                        break
+            print()
 
 if __name__ == "__main__":
-  main()
+    pyramid(4)
