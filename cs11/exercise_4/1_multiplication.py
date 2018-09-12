27405068cc2801a5bbfe393005da91e86ef68548
mimin <optimusupperman@gmail.com>
2018-09-12 08:32:26 +0800

    bugged

--- cs11/exercise_4/1_multiplication.py
+++ cs11/exercise_4/1_multiplication.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 
 def main():
-  t = int(input())
-  if t <= 0:
-    pass # temporary
-  else:
-    # variable declarations
-    array = []
-    n = 0
-    for i in range(t):
-      n = int(input())
-      if n <= 0:
-        return
-      else:
-        array.append(n)
-    
-    for element in array:
-      plot(element)
+    t = int(input())
+    if t <= 0:
+        pass # temporary
+    else:
+        # variable declarations
+        array = []
+        n = 0
+        for i in range(t):
+            n = int(input())
+            if n <= 0:
+                return
+            else:
+                array.append(n)
+
+        for element in array:
+            plot(element)
 
 def plot(dimension):
-  for i in range(1, dimension + 1):
-    for k in range(1, dimension + 1):
-      print(i * k, end='\t')
-    print('\n')
-  print('\n')
+    for i in range(1, dimension + 1):
+        for k in range(1, dimension + 1):
+            print(str(i * k), end='')
+            if (k != dimension):
+                print('\t', end='')
+        print()
+    print()
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- cs11/exercise_4/3_pyramid.py
+++ cs11/exercise_4/3_pyramid.py
@@ -3,42 +3,48 @@
 from math import ceil
 
 def main():
-  t = int(input())
-  if t <= 0:
-    pass # temporary
-  else:
-    # variable declarations
-    array = []
-    h = 0
-    for i in range(t):
-      h = int(input())
-      if h <= 0:
-        return
-      else:
-        array.append(h)
+    t = int(input())
+    if t <= 0:
+        pass # temporary
+    else:
+        # variable declarations
+        array = []
+        h = 0
+        for i in range(t):
+            h = int(input())
+            if h <= 0:
+                return
+            else:
+                array.append(h)
 
-    for element in array:
-      pyramid(element)
+        for element in array:
+            pyramid(element)
 
 def pyramid(dimension):
-  width = dimension * 2 - 1
-  total_width = width ** 2
-  total_mid = ceil(total_width / 2)
-  middles = [total_mid]
+    width = dimension * 2 - 1
+    total_width = width ** 2
+    total_mid = ceil(total_width / 2)
+    middles = []
+    center = dimension
+    while center < total_width:
+        middles.append(center)
+        center = center + width
 
-  for big_row in range(dimension):
-    for row in range(dimension):
-      for col in range(total_width):
-        for element in middles:
-          if element - 1 - row <= col <= element - 1 + row:
-            print('*', end="")
-          else:
-            print(' ', end="")
-      print(' ')
+    for big_row in range(dimension):
+        for row in range(dimension):
+            for col in range(total_width):
+                for center in middles:
+                    if col < center - 1 - row:
+                        print(' ', end='')
+                    elif col <= center - 1 + row:
+                        print('*', end='')
+            print()
 
-    if big_row != 0:
-      middles.append(total_mid - big_row * width)
-      middles.append(total_mid + big_row * width)
+    # DEBUG
+    print("width: " + str(width))
+    print("total_width: " + str(total_width))
+    print("total_mid: " + str(total_mid))
+    print(middles)
 
 if __name__ == "__main__":
-  main()
+    pyramid(2)
