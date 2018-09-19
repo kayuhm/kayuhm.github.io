4f53d31b79320838e8fcf347f8637fc1bbadb2ff
mimin <42409359+mimin@users.noreply.github.com>
2018-09-19 10:42:39 +0800

    Update 3_polynomial.py

--- cs11/exercise_5/3_polynomial.py
+++ cs11/exercise_5/3_polynomial.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env env python3
 
+def main():
+    t = int(input())
+    if t <= 0:
+        # saved for invalid input output
+        pass
+    else:
+        arrPx = []
+        arrQx = []
+        for i in range(t):
+            arrPx.append(input().split())
+            arrQx.append(input().split())
 
-t = int(input())
-if t <= 0:
-    # saved for invalid input output
-    pass
-else:
-    arrPx = []
-    arrQx = []
-    for i in range(t):
-        arrPx.append(input().split())
-        arrQx.append(input().split())
-
-    for i in range(len(arrPx)):
-        polyAdd(arrPx[i], arrQx[i])
+        for i in range(len(arrPx)):
+            polyAdd(arrPx[i], arrQx[i])
 
 def polyAdd(P_POLY, Q_POLY):
-    difference = 0
-    biggest = 0
-    if len(P_POLY) > len(Q_POLY):
-        biggest = len(P_POLY)
-        difference = biggest - len(Q_POLY) - 1
-        for i in range(1, difference + 1):
-            print(str(P_POLY[i]) + "x^" + str(P_POLY[0]))
-            P_POLY[0] -= 1
+    p_deg = int(P_POLY.pop(0))
+    q_deg = int(Q_POLY.pop(0))
+    p_len = len(P_POLY)
+    q_len = len(Q_POLY)
+    if (p_len > q_len):
+        for i in range(p_len - q_len):
+            print(str(P_POLY.pop(p_len - 1)) + "x^" + str(p_deg), end=" + ")
+            p_len -= 1
+            p_deg -= 1
     else:
-        biggest = len(Q_POLY)
-        difference = biggest - len(P_POLY) - 1
-        for i in range(1, difference + 1):
-            print(str(Q_POLY[i]) + "x^" + str(Q_POLY[0]))
-            Q_POLY[0] -= 1
-    for index in range(difference + 1, ):
-
+        for i in range(q_len - p_len):
+            print(str(Q_POLY.pop(q_len - 1)) + "x^" + str(q_deg), end=" ")
+            q_len -= 1
+            q_deg -= 1
+    for index in range(p_len - 1, -1, -1):
+        print(str(int(P_POLY[index]) + int(Q_POLY[index])) + "x^" + str(p_deg), end="")
+        if p_deg != 0:
+            print(" + ", end="")
+        else:
+            print()
+        p_deg -= 1
 
 if __name__ == "__main__":
     main()
