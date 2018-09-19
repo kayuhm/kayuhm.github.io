afb3926649318b34757d8b9d5bc8156873b92c4a
mimin <42409359+mimin@users.noreply.github.com>
2018-09-19 10:48:35 +0800

    Update 3_polynomial.py

--- cs11/exercise_5/3_polynomial.py
+++ cs11/exercise_5/3_polynomial.py
@@ -22,21 +22,28 @@ def polyAdd(P_POLY, Q_POLY):
     q_len = len(Q_POLY)
     if (p_len > q_len):
         for i in range(p_len - q_len):
-            print(str(P_POLY.pop(p_len - 1)) + "x^" + str(p_deg), end=" + ")
+            if (P_POLY[-1] != 0):
+                print(str(P_POLY.pop(p_len - 1)) + "x^" + str(p_deg), end=" + ")
             p_len -= 1
             p_deg -= 1
     else:
         for i in range(q_len - p_len):
-            print(str(Q_POLY.pop(q_len - 1)) + "x^" + str(q_deg), end=" ")
+            if (Q_POLY[-1] != 0):
+                print(str(Q_POLY.pop(q_len - 1)) + "x^" + str(q_deg), end=" ")
             q_len -= 1
             q_deg -= 1
     for index in range(p_len - 1, -1, -1):
-        print(str(int(P_POLY[index]) + int(Q_POLY[index])) + "x^" + str(p_deg), end="")
-        if p_deg != 0:
-            print(" + ", end="")
-        else:
-            print()
+        if (int(P_POLY[index]) + int(Q_POLY[index]) != 0):
+            print(str(int(P_POLY[index]) + int(Q_POLY[index])) + "x^" + str(p_deg), end="")
+            if p_deg != 0:
+                print(" + ", end="")
+            else:
+                print()
         p_deg -= 1
+
+        
+
+
 
 if __name__ == "__main__":
     main()
