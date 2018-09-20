87d4b1897ccf854cade35204b407504f5503f8b7
mimin <42409359+mimin@users.noreply.github.com>
2018-09-20 15:10:19 +0800

    Update 5_histogram.py

--- cs11/exercise_5/5_histogram.py
+++ cs11/exercise_5/5_histogram.py
@@ -1,25 +1,40 @@
+#!/usr/bin/env python3
+
 def main():
     t = int(input())
     if t <= 0:
+        # saved for error
         pass
     else:
-        arrInput = []
-        for i in range(t):
-            arrInput.append((input()).split())
-        for element in arrInput:
-            printLargest(element)
+        b = []
+        data = []
+        for count in range(t):
+            b.append(int(input()))
+            data.append(input().split())
+        for index in range(len(b)):
+            hist(b[index], data[index])
+         
 
-        
+def hist(BIN_SIZE, DATA_LIST):
+    DATA_LIST.sort()
+    locBinSizeOffset = int(BIN_SIZE) - 1
+    locLowerBound = [int(DATA_LIST[0])]
+    locHigherBound = [locLowerBound[0] + locBinSizeOffset]
+    locFrequency = []
 
-def printLargest(ARRAY_OF_WORDS):
-    if len(ARRAY_OF_WORDS) == 1:
-        print(ARRAY_OF_WORDS[0])
-    else:
-        largest = 0
-        for index in range(1,len(ARRAY_OF_WORDS)):
-            if len(ARRAY_OF_WORDS[index]) > len(ARRAY_OF_WORDS[largest]):
-                largest = index
-        print(ARRAY_OF_WORDS[largest])    
+    while (int(DATA_LIST[-1]) > locHigherBound[-1]):
+        locLowerBound.append(locLowerBound[-1] + locBinSizeOffset)
+        locHigherBound.append(locHigherBound[-1] + locBinSizeOffset)
+
+    # debugging
+    print(DATA_LIST)
+    print(locLowerBound)
+    print(locHigherBound)
+    # end debugging 
+
+    
+
+
 
 if __name__ == "__main__":
-  main()
+    main()
