357f0d553bd52333db84d2ea55c7bc287cf37423
mimin <42409359+mimin@users.noreply.github.com>
2018-09-20 15:39:19 +0800

    Update 5_histogram.py

--- cs11/exercise_5/5_histogram.py
+++ cs11/exercise_5/5_histogram.py
@@ -16,34 +16,43 @@ def main():
          
 
 def hist(BIN_SIZE, DATA_LIST):
+    # make all data in DATA_LIST an int
+    counter = 0
+    while counter < len(DATA_LIST):
+        DATA_LIST[counter] = int(DATA_LIST[counter])
+        counter += 1
     DATA_LIST.sort()
     locBinSizeOffset = int(BIN_SIZE) - 1
-    locLowerBound = [int(DATA_LIST[0])]
-    locHigherBound = [locLowerBound[0] + locBinSizeOffset]
-    locFrequency = []
+    locLowerBound = [DATA_LIST[0]]
+    locHigherBound = [locLowerBound[0] + locBinSizeOffset + 1]
+    locFrequencyList = []
 
-    while (int(DATA_LIST[-1]) > locHigherBound[-1]):
+    while (DATA_LIST[-1] > locHigherBound[-1]):
         locLowerBound.append(locLowerBound[-1] + locBinSizeOffset)
         locHigherBound.append(locHigherBound[-1] + locBinSizeOffset)
-
-    frequency = 0
+    
     indexOfBounds = 0
-    for index in range(len(DATA_LIST) - 1):
-        if locLowerBound[indexOfBounds] < int(DATA_LIST[index]) < locHigherBound[indexOfBounds]:
+    frequency = 0
+    while len(DATA_LIST) != 0:
+        if locLowerBound[indexOfBounds] <= DATA_LIST[0] <= locHigherBound[indexOfBounds]:
             frequency += 1
+            DATA_LIST.pop(0)
         else:
-            locFrequency.append(frequency)
+            locFrequencyList.append(frequency)
             frequency = 0
             indexOfBounds += 1
+    
+    # display result
+    for index in range(len(locFrequencyList)):
+        print(str(locLowerBound[index]) + "-" + str(locHigherBound[index]), str(locFrequencyList[index]))
 
 
     # debugging
-    print(DATA_LIST)
-    print(locLowerBound)
-    print(locHigherBound)
+    # print(DATA_LIST)
+    # print(locFrequencyList)
+    # print(locLowerBound)
+    # print(locHigherBound)
     # end debugging 
-
-    
 
 
 
